41eb9ba33153ee8e054bf17e54db28f1ade30e3f
Daniel Chaves <85415599+lilkirito22@users.noreply.github.com>
2023-11-19 16:51:32 -0300

    First Commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  height: 60px;
+`
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+`
+const Left = styled.div`
+  width: 33.3%;
+`
+const Center = styled.div`
+  width: 33.3%;
+`
+const Right = styled.div`
+  width: 33.3%;
+`
+
+const Navbar = () => {
+  return (
+    <div>
+      <Container>
+        <Wrapper>
+          <Left>Left</Left>
+          <Center>Center</Center>
+          <Right>Right</Right>
+        </Wrapper>
+      </Container>
+    </div>
+  )
+}
+
+export default Navbar
